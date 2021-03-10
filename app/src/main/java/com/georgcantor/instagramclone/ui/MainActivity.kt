b0b4edb498365d41466ec9995e8ed55615ecfea7
GeorgCantor <cupsman1986@gmail.com>
2021-03-10 16:08:15 +0300

    Add NavigationExtensions

--- app/src/main/java/com/georgcantor/instagramclone/ui/MainActivity.kt
+++ app/src/main/java/com/georgcantor/instagramclone/ui/MainActivity.kt
@@ -1,13 +1,51 @@
 package com.georgcantor.instagramclone.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.navigation.NavController
 import com.georgcantor.instagramclone.R
+import com.georgcantor.instagramclone.databinding.ActivityMainBinding
+import com.georgcantor.instagramclone.util.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private var currentNavController: LiveData<NavController>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) setupBottomNavigationBar()
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        setupBottomNavigationBar()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return currentNavController?.value?.navigateUp() ?: false
+    }
+
+    private fun setupBottomNavigationBar() {
+        val navGraphIds = listOf(
+            R.navigation.home,
+            R.navigation.search,
+            R.navigation.add,
+            R.navigation.favorite,
+            R.navigation.profile
+        )
+
+        val controller = binding.bottomNav.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.fragment_container_view,
+            intent = intent
+        )
+
+        currentNavController = controller
     }
 }
--- app/src/main/java/com/georgcantor/instagramclone/ui/home/HomeFragment.kt
+++ app/src/main/java/com/georgcantor/instagramclone/ui/home/HomeFragment.kt
@@ -4,10 +4,19 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.georgcantor.instagramclone.R
+import com.georgcantor.instagramclone.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
+    private var binding: FragmentHomeBinding? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHomeBinding.bind(view)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
