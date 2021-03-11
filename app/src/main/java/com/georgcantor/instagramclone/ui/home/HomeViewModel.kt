c9ad0416b4852a9a70bad2aa68fef0c7e8d0839a
GeorgCantor <cupsman1986@gmail.com>
2021-03-11 15:29:11 +0300

    Add exception handler

--- app/src/main/java/com/georgcantor/instagramclone/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/georgcantor/instagramclone/ui/home/HomeViewModel.kt
@@ -5,14 +5,20 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.georgcantor.instagramclone.model.Repository
 import com.georgcantor.instagramclone.model.response.Hit
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.launch
 
 class HomeViewModel(repository: Repository) : ViewModel() {
 
     val pictures = MutableLiveData<List<Hit>>()
+    val error = MutableLiveData<String>()
+
+    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        error.postValue(throwable.message)
+    }
 
     init {
-        viewModelScope.launch {
+        viewModelScope.launch(exceptionHandler) {
             repository.getPictures().apply {
                 if (isSuccessful) pictures.postValue(body()?.hits)
             }
